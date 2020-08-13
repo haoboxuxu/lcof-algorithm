b5a48c271fbe7e68efb2bc87904b7719c31f9efb
徐浩博 <xuhaobo@xuhaobodeMacBook-Pro.local>
2020-08-13 14:50:42 +0800

    create

---
+++ 9_用两个栈实现队列.java
@@ -0,0 +1,32 @@
+class CQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        }else {
+            return -1;
+        }
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
