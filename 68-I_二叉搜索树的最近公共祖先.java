64900c65914270b35366c88caeb043fc39c1452b
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-08-20 16:48:17 +0800

    Create 68-I_二叉搜索树的最近公共祖先.java

---
+++ 68-I_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// 6ms
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
+// 6ms
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            }else if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            }else {
+                break;
+            }
+        }
+        return root;
+    }
+}
