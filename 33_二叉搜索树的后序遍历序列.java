d1f147e72a513c3795c39309c29c2e1c067792fb
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-08-30 16:19:34 +0800

    Create 33_二叉搜索树的后序遍历序列.java

---
+++ 33_二叉搜索树的后序遍历序列.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean verifyPostorder(int[] postorder) {
+        return dfs(postorder, 0, postorder.length - 1);
+    }
+
+    public boolean dfs(int[] postorder, int i, int j) {
+        if (i >= j) {
+            return true;
+        }
+        int p = i;
+        while (postorder[p] < postorder[j]) {
+            p++;
+        }
+        int m = p;
+        while (postorder[p] > postorder[j]) {
+            p++;
+        }
+        return p == j && dfs(postorder, i, m-1) && dfs(postorder, m, j-1);
+    }
+}
